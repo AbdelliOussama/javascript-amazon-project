b6369a425c6364015f9f44ada7426dcafdd238d7
AbdelliOussama <oussema.abdelli@iteam-univ.tn>
2025-11-05 07:52:02 +0100

    Learn Promises and convert code into Promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,50 @@
 import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-oop.js';
 // import'../data/cart-class.js';
-loadProducts(() => {
+
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(()=>{
     renderCartSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderCartSummary();
+    renderPaymentSummary();
+});
+
+*/
+
+
+
+// loadProducts(() => {
+//     renderCartSummary();
+//     renderPaymentSummary();
+// });
